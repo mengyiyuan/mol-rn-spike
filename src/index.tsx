394b4acc8a023b9c27adb4a68f91ac725d341145
Mengyi Yuan <myuan@thoughtworks.com>
2017-10-06 21:09:58 +0800

    Finish migrating ios to typescript

---
+++ src/index.tsx
@@ -0,0 +1,46 @@
+import { AppRegistry, StyleSheet, Text, View } from 'react-native'
+
+import React from 'react'
+
+export interface Props { }
+export interface State { }
+
+const styles: any = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5
+  }
+})
+
+export default class MolSpike extends React.Component<Props, State> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Hello World!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    )
+  }
+}
+
+AppRegistry.registerComponent('MolSpike', () => MolSpike)
